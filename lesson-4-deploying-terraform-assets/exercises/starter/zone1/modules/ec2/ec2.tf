e9e1b71438183000a21c9baf00e1bf4350f29b19
marcolcal <marcolcal@gmail.com>
2024-09-29 11:50:38 -0400

    added needed arguments to the module vpc and eec2

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -1,15 +1,20 @@
 resource "aws_instance" "ubuntu" {
-  ami           = 
-  count = 
-  instance_type = 
+  ami           = var.aws_ami
+  count         = var.instance_count
+  instance_type = "t3.micro"
   subnet_id = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = ""
+    Name = "udacity-web"
   }
 }
 
 resource "aws_security_group" "ec2_sg" {
- 
+    name    = "ec2_sg"
+    vpc_id  = var.vpc_id
+
+  tags = {
+    Name = "ec2_sg"
+  }
 }
