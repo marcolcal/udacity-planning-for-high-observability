b33d3755544435910af2b5a7bf89260f219dfbf3
marcolcal <marcolcal@gmail.com>
2024-09-29 12:13:02 -0400

    added the security group reqs

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -13,6 +13,28 @@ resource "aws_instance" "ubuntu" {
 resource "aws_security_group" "ec2_sg" {
     name    = "ec2_sg"
     vpc_id  = var.vpc_id
+    
+  ingress {    
+    description = "web port"
+    from_port   = 80    
+    to_port     = 80
+    protocol    = "tcp"    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh port"
+    from_port   = 22    
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "ec2_sg"
