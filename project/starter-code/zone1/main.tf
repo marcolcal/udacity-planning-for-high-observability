9e72648b33169485f7d0ba03e72c24d1b3e0f2b3
marcolcal <marcolcal@gmail.com>
2024-10-01 15:41:45 -0400

    Adding some bs code 4

--- project/starter-code/zone1/main.tf
+++ project/starter-code/zone1/main.tf
@@ -53,15 +53,3 @@ output "vpc_id" {
  output "public_subnet_ids" {
    value = module.vpc.public_subnet_ids
  }
-
-output "vpc_id" {
-   value = module.vpc_west.vpc_id
- }
-
- output "private_subnet_ids" {
-   value = module.vpc_west.private_subnet_ids
- }
-
- output "public_subnet_ids" {
-   value = module.vpc_west.public_subnet_ids
- }
